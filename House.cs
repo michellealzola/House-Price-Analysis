03178d10c57934c2f9475f563b630ec10f4c31a5
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-27 07:26:33 -0600

    Updated House class

--- House.cs
+++ House.cs
@@ -8,5 +8,22 @@ namespace House_Price_Analysis
 {
     internal class House
     {
+        public House(decimal price, int bedroom, int bathroom, double squareFeet)
+        {
+            Price = price;
+            Bedroom = bedroom;
+            Bathroom = bathroom;
+            SquareFeet = squareFeet;
+        }
+
+        public decimal Price { get; set;}
+
+        public int Bedroom { get; set;}
+
+        public int Bathroom { get; set;}
+
+        public double SquareFeet { get; set;}
+
+
     }
 }
