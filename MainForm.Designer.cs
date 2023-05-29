0e4fa2303384b481e12aab1cc7ae71c8bc22543c
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-29 07:09:14 -0600

    Radio Button

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,36 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lBoxHouseList = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBedroom5 = new System.Windows.Forms.RadioButton();
+            this.radioBedroom34 = new System.Windows.Forms.RadioButton();
+            this.radioBedroom12 = new System.Windows.Forms.RadioButton();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.radioButton3 = new System.Windows.Forms.RadioButton();
             this.radioButton4 = new System.Windows.Forms.RadioButton();
             this.radioButton5 = new System.Windows.Forms.RadioButton();
             this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
             this.radioButton7 = new System.Windows.Forms.RadioButton();
             this.radioButton8 = new System.Windows.Forms.RadioButton();
             this.radioButton9 = new System.Windows.Forms.RadioButton();
-            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lBoxHouseList
             // 
-            this.listBox1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 26;
-            this.listBox1.Location = new System.Drawing.Point(21, 45);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(612, 160);
-            this.listBox1.TabIndex = 0;
+            this.lBoxHouseList.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBoxHouseList.FormattingEnabled = true;
+            this.lBoxHouseList.ItemHeight = 26;
+            this.lBoxHouseList.Location = new System.Drawing.Point(21, 45);
+            this.lBoxHouseList.Name = "lBoxHouseList";
+            this.lBoxHouseList.Size = new System.Drawing.Size(612, 160);
+            this.lBoxHouseList.TabIndex = 0;
             // 
             // label1
             // 
@@ -71,9 +71,9 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.radioButton3);
-            this.groupBox1.Controls.Add(this.radioButton2);
-            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioBedroom5);
+            this.groupBox1.Controls.Add(this.radioBedroom34);
+            this.groupBox1.Controls.Add(this.radioBedroom12);
             this.groupBox1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(20, 220);
             this.groupBox1.Name = "groupBox1";
@@ -81,6 +81,39 @@
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Bedrooms";
+            // 
+            // radioBedroom5
+            // 
+            this.radioBedroom5.AutoSize = true;
+            this.radioBedroom5.Location = new System.Drawing.Point(22, 112);
+            this.radioBedroom5.Name = "radioBedroom5";
+            this.radioBedroom5.Size = new System.Drawing.Size(47, 30);
+            this.radioBedroom5.TabIndex = 2;
+            this.radioBedroom5.TabStop = true;
+            this.radioBedroom5.Text = "5+";
+            this.radioBedroom5.UseVisualStyleBackColor = true;
+            // 
+            // radioBedroom34
+            // 
+            this.radioBedroom34.AutoSize = true;
+            this.radioBedroom34.Location = new System.Drawing.Point(22, 76);
+            this.radioBedroom34.Name = "radioBedroom34";
+            this.radioBedroom34.Size = new System.Drawing.Size(62, 30);
+            this.radioBedroom34.TabIndex = 1;
+            this.radioBedroom34.TabStop = true;
+            this.radioBedroom34.Text = "3 - 4";
+            this.radioBedroom34.UseVisualStyleBackColor = true;
+            // 
+            // radioBedroom12
+            // 
+            this.radioBedroom12.AutoSize = true;
+            this.radioBedroom12.Location = new System.Drawing.Point(22, 40);
+            this.radioBedroom12.Name = "radioBedroom12";
+            this.radioBedroom12.Size = new System.Drawing.Size(60, 30);
+            this.radioBedroom12.TabIndex = 0;
+            this.radioBedroom12.TabStop = true;
+            this.radioBedroom12.Text = "1 - 2";
+            this.radioBedroom12.UseVisualStyleBackColor = true;
             // 
             // groupBox2
             // 
@@ -94,63 +127,6 @@
             this.groupBox2.TabIndex = 0;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Bathrooms";
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.radioButton10);
-            this.groupBox3.Controls.Add(this.radioButton7);
-            this.groupBox3.Controls.Add(this.radioButton8);
-            this.groupBox3.Controls.Add(this.radioButton9);
-            this.groupBox3.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox3.Location = new System.Drawing.Point(432, 220);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(200, 203);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Area (sq. ft.)";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(21, 451);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(612, 43);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Search Price";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(22, 40);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(60, 30);
-            this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "1 - 2";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // radioButton2
-            // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(22, 76);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(62, 30);
-            this.radioButton2.TabIndex = 1;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "3 - 4";
-            this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // radioButton3
-            // 
-            this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(22, 112);
-            this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(47, 30);
-            this.radioButton3.TabIndex = 2;
-            this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "5+";
-            this.radioButton3.UseVisualStyleBackColor = true;
             // 
             // radioButton4
             // 
@@ -185,6 +161,31 @@
             this.radioButton6.Text = "1 - 2";
             this.radioButton6.UseVisualStyleBackColor = true;
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioButton10);
+            this.groupBox3.Controls.Add(this.radioButton7);
+            this.groupBox3.Controls.Add(this.radioButton8);
+            this.groupBox3.Controls.Add(this.radioButton9);
+            this.groupBox3.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(432, 220);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 203);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Area (sq. ft.)";
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(21, 148);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(76, 30);
+            this.radioButton10.TabIndex = 5;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "1500+";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
             // radioButton7
             // 
             this.radioButton7.AutoSize = true;
@@ -218,16 +219,15 @@
             this.radioButton9.Text = "below 500";
             this.radioButton9.UseVisualStyleBackColor = true;
             // 
-            // radioButton10
+            // button1
             // 
-            this.radioButton10.AutoSize = true;
-            this.radioButton10.Location = new System.Drawing.Point(21, 148);
-            this.radioButton10.Name = "radioButton10";
-            this.radioButton10.Size = new System.Drawing.Size(76, 30);
-            this.radioButton10.TabIndex = 5;
-            this.radioButton10.TabStop = true;
-            this.radioButton10.Text = "1500+";
-            this.radioButton10.UseVisualStyleBackColor = true;
+            this.button1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 451);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(612, 43);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search Price";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
@@ -239,7 +239,7 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lBoxHouseList);
             this.Name = "MainForm";
             this.Text = "House Price Analysis";
             this.groupBox1.ResumeLayout(false);
@@ -255,15 +255,15 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lBoxHouseList;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RadioButton radioButton3;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioBedroom5;
+        private System.Windows.Forms.RadioButton radioBedroom34;
+        private System.Windows.Forms.RadioButton radioBedroom12;
         private System.Windows.Forms.RadioButton radioButton4;
         private System.Windows.Forms.RadioButton radioButton5;
         private System.Windows.Forms.RadioButton radioButton6;
